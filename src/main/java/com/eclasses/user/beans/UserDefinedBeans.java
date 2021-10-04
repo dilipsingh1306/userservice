34d029cbf1c4613421b7cbd18d99585f76a163e4
Dilip Singh <dilipsingh1306@gmail.com>
2021-10-04 18:59:17 +0530

    security

---
+++ src/main/java/com/eclasses/user/beans/UserDefinedBeans.java
@@ -0,0 +1,16 @@
+package com.eclasses.user.beans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class UserDefinedBeans {
+
+	@Bean
+	public BCryptPasswordEncoder getBCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+
+	}
+
+}
